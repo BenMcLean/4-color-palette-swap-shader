c0ca54d65a188c913fafe18e64c76494a5b76a12
Benjamin McLean <mclean.ben@gmail.com>
2016-12-02 17:19:04 -0600

    put the shader code in-line

--- core/src/main/java/FourColorShaderDemo/FirstScreen.java
+++ core/src/main/java/FourColorShaderDemo/FirstScreen.java
@@ -36,18 +36,35 @@ public class FirstScreen implements Screen {
     @Override
     public void show() {
         palette = new Color[4];
-        palette[0] = new Color(156/255f,189/255f,15/255f,255/255f);
-        palette[1] = new Color(140/255f,173/255f,15/255f,255/255f);
-        palette[2] = new Color(48/255f,98/255f,48/255f,255/255f);
-        palette[3] = new Color(15/255f,56/255f,15/255f,255/255f);
+        palette[0] = new Color(156 / 255f, 189 / 255f, 15 / 255f, 255 / 255f);
+        palette[1] = new Color(140 / 255f, 173 / 255f, 15 / 255f, 255 / 255f);
+        palette[2] = new Color(48 / 255f, 98 / 255f, 48 / 255f, 255 / 255f);
+        palette[3] = new Color(15 / 255f, 56 / 255f, 15 / 255f, 255 / 255f);
 
-        shader  = new ShaderProgram(Gdx.files.internal("shaders/VertexShader.glsl"), Gdx.files.internal("shaders/FragmentShader.glsl"));
-        //shader.pedantic = false;
+        shader = new ShaderProgram(
+                "#version 120\nattribute vec4 a_position;\nattribute vec4 a_color;\nattribute vec2 a_texCoord0;\nuniform mat4 u_projTrans;\nvarying vec4 v_color;\nvarying vec2 v_texCoords;\n\nvoid main() {\n    v_color = a_color;\n    v_color.a = v_color.a * 1.0039216; //* (256.0/255.0)\n    v_texCoords = a_texCoord0;\n    gl_Position = u_projTrans * a_position;\n}"
+                ,
+                "#version 120\n" +
+                        "#ifdef GL_ES\n" +
+                        "#define LOWP lowp\n" +
+                        "precision mediump float;\n" +
+                        "#else\n" +
+                        "#define LOWP\n" +
+                        "#endif\n\n" +
+                        "varying LOWP vec4 v_color;\n" +
+                        "varying vec2 v_texCoords;\n" +
+                        "uniform vec4 u_palette[4];\n" +
+                        "uniform sampler2D u_texture;\n\n" +
+                        "void main() {\n" +
+                        "    gl_FragColor = u_palette[int(texture2D(u_texture, v_texCoords).r * 3.9999)];\n" +
+                        "}"
+        );
+        
         if (!shader.isCompiled()) throw new GdxRuntimeException("Couldn't compile shader: " + shader.getLog());
 
         int location = shader.getUniformLocation("u_palette[0]");
         Gdx.app.debug("location", Integer.toString(location));
-        for (int x=0; x<palette.length; x++)
+        for (int x = 0; x < palette.length; x++)
             shader.setUniformf(location + x, palette[x]);
 
         atlas = new TextureAtlas("art.atlas");
