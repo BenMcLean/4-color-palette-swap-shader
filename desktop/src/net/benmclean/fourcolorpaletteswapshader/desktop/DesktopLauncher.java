6391b947f5c1ddf5b5c27e57a63a7c43aba7037d
Benjamin McLean <mclean.ben@gmail.com>
2016-12-13 08:28:54 -0600

    Created new libGDX project

---
+++ desktop/src/net/benmclean/fourcolorpaletteswapshader/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.benmclean.fourcolorpaletteswapshader.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.benmclean.fourcolorpaletteswapshader.FourColorPaletteSwapShaderGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FourColorPaletteSwapShaderGame(), config);
+	}
+}
